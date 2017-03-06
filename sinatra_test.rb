31df274bbdbd6e1ab3a79a4ebf8523e11211c559
Michael Carter <mpcarter76@gmail.com>
2017-03-06 23:26:24 +0000

    Working with ExchangeRate.

--- sinatra_test.rb
+++ sinatra_test.rb
@@ -1,12 +1,15 @@
 require 'sinatra'
+require 'exchange_rate'
 
 get "/fxrate" do
   @date = params['date'].nil? ? Date.today : params['date']
   @amount = params['amount']
   @from_cur = params['from_cur']
   @to_cur = params['to_cur']
-  #fx_rate = ExchangeRate.at(@date, @from_cur, @to_cur)
-  #@conversion_result = (@amount.to_f * fx_rate).round(3)
+  unless @from_cur.nil? || @to_cur.nil?
+    fx_rate = ExchangeRate.at(@date, @from_cur, @to_cur)
+    @conversion_result = (@amount.to_f * fx_rate).round(3)
+  end
   erb :fx_rates
 end
 
